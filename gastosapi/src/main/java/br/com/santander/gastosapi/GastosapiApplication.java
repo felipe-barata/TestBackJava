8ed895931ec19a5da346b02e7abaa0d96336db16
felipe-barata <felipeabarata@gmail.com>
2020-07-04 13:14:33 -0300

    Criação do projeto no maven

---
+++ gastosapi/src/main/java/br/com/santander/gastosapi/GastosapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.santander.gastosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastosapiApplication.class, args);
+	}
+
+}
---
+++ gastosapi/src/test/java/br/com/santander/gastosapi/GastosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.santander.gastosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
